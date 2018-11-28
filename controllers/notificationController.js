1f0468008d1c20d46b6c92da2e1d29dd04992392
cristian7x <cristian.camilo.2594@gmail.com>
2018-11-28 01:20:02 -0500

    In-app Notifications

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -1,31 +1,33 @@
-const Notification  = require('../models/notification');
-const User          = require('../models/user');
+const mongoose = require('mongoose');
+const Notification = mongoose.model('Notification');
+const User = mongoose.model('User');
 
 // Display all notifications on GET
-exports.notificationIndex = async function(req, res){
-  try {
-    let user = await User.findById(req.params.id).populate({
-      path: 'notifications',
-      options: { sort: { "_id": -1 } }
-    }).exec();
-    let notificationsList = user.notifications;
-    res.render('notifications/index', { notificationsList });
-  } catch(err) {
-    req.flash('error', err.message);
-    res.redirect('back');
-  }
+exports.getNotifications = async (req, res) => {
+  const user = await User.findById(req.params.id).populate({
+    path: 'notifications',
+    populate: { path: 'user' },
+    options: { sort: { "_id": -1 } }
+  });
+  res.render('notifications/index', { notificationsList: user.notifications });
 };
 
 // Display detail page for a specific notification on GET
-exports.notificationShow = async function(req, res){
-  try {
-    let notification = await Notification.findById(req.params.notification_id);
-    notification.isRead = true;
-    notification.save();
-    const route = notification.landscapeId ? `/landscapes/${notification.landscapeId}` : `/users/${notification.userId}`;
-    res.redirect(route);
-  } catch(err) {
-    req.flash('error', err.message);
-    res.redirect('back');
+exports.showNotification = async (req, res) => {
+  const notification = await Notification.findOneAndUpdate( { _id: req.params.notification_id }, { isRead: true }, { new: true });
+  const route = notification.landscape ? `/landscapes/${notification.landscape}` : `/users/${notification.user}`;
+  res.redirect(route);
+};
+
+// Get notifications no read
+exports.getNotificacionsNotRead = async (user) => {
+  if (!user) {
+    return undefined;
   }
+  const { notifications } = await User.findById(user._id).populate({
+    path: 'notifications',
+    populate: { path: 'user' },
+    match: { isRead: false }
+  }).exec();
+  return notifications.reverse();
 };
